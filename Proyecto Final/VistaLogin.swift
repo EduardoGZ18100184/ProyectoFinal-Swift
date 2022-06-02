6787f687cd26d648a8262bfe6b971b903e0fcabb
Eduardo MacOs <L18100184@nlaredo.tecnm.mx>
2022-06-01 22:13:57 -0500

    Agregando imagenes al inicio

--- Proyecto Final/VistaLogin.swift
+++ Proyecto Final/VistaLogin.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class VistaLogin: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
